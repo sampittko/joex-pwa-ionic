2878ed2d5317525bbd16377fc5db0417a39e0b20
Samuel Pitonak <sampittko@gmail.com>
2024-11-13 14:05:43 +0100

    chore: pwa

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,20 @@
 import legacy from '@vitejs/plugin-legacy'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa';
+import mkcert from'vite-plugin-mkcert'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    legacy()
+    legacy(),
+    mkcert(),
+    VitePWA({ registerType: 'autoUpdate' })
   ],
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/setupTests.ts',
-  }
+  },
 })
