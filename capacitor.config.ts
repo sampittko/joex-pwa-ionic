5dd7cdde13d491aba7a6e6aa0e54ac8b93eae896
Samuel Pitonak <sampittko@gmail.com>
2024-11-13 19:26:34 +0100

    chore: updates app id

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,9 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'joex',
-  webDir: 'dist'
+  appId: "app.sampittko.joex",
+  appName: "joex",
+  webDir: "dist",
 };
 
 export default config;
