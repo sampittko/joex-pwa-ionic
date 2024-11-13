81fed03f34a55e2b92bd1155e651882a27d52ebf
Samuel Pitonak <sampittko@gmail.com>
2024-11-13 19:46:36 +0100

    chore: updates app name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: "app.sampittko.joex",
-  appName: "joex",
+  appName: "Joex",
   webDir: "dist",
   plugins: {
     Badge: {
