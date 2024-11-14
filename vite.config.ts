a297ebdd915e5a1a402eeb2d0e3f0084d8d12e6f
Samuel Pitonak <sampittko@gmail.com>
2024-11-14 14:19:14 +0100

    feat: import alias config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import legacy from '@vitejs/plugin-legacy'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import { VitePWA } from "vite-plugin-pwa";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -69,5 +70,10 @@ export default defineConfig({
     globals: true,
     environment: "jsdom",
     setupFiles: "./src/setupTests.ts",
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 });
